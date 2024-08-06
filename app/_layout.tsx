2875f3e158dd1ab1fa2d6ee49dc36932e8fa0434
EMMAN-PAPPS-L14\Emman <emman@puttiapps.com>
2024-08-06 20:36:56 +0800

    feat: initial commits for react native project

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
