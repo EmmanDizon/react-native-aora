e975d66cd6b475b87542ed9fb579038634718fd2
EMMAN-PAPPS-L14\Emman <emman@puttiapps.com>
2024-08-06 21:19:11 +0800

    feat: list of fonts

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello There emman !</Text>
+      <Text className="text-3xl font-black">Hello There emman !</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
