40ed9c3c1a10188d3986030c0f5fbde4df0c756d
EMMAN-PAPPS-L14\Emman <emman@puttiapps.com>
2024-08-06 22:46:04 +0800

    feat: component tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,116 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, ImageSourcePropType } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
+interface ITabIcon {
+  icon: ImageSourcePropType;
+  color: string;
+  name: string;
+  focused: boolean;
+}
 
+const TabIcon = ({ icon, color, focused, name }: ITabIcon) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-semibold" : "font-normal"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 83,
+          },
+        }}
+      >
+        {/* Home screen*/}
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                focused={focused}
+                name="Home"
+              />
+            ),
+          }}
+        ></Tabs.Screen>
+
+        {/* Bookmark*/}
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                focused={focused}
+                name="Bookmark"
+              />
+            ),
+          }}
+        ></Tabs.Screen>
+
+        {/* Create*/}
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                focused={focused}
+                name="Create"
+              />
+            ),
+          }}
+        ></Tabs.Screen>
+
+        {/* Profile*/}
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                focused={focused}
+                name="Profile"
+              />
+            ),
+          }}
+        ></Tabs.Screen>
+      </Tabs>
+    </>
   );
 };
 
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Pogi si emman</Text>
     </View>
   );
 };
