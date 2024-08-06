df74c33f02a9f2ddeef70a89296dae419997b131
EMMAN-PAPPS-L14\Emman <emman@puttiapps.com>
2024-08-06 21:36:22 +0800

    refactor: remove unused code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from "react-native";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
@@ -34,12 +33,3 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
